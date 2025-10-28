00c718a420374e76c9c80be216ebfb7b99c7ea72
Roberto Bardales <rebv.start@gmail.com>
2025-10-27 23:09:01 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    print("Iniciando Flask...")
+    app.run(debug=True, host='127.0.0.1', port=5001)
+    print("Flask iniciado")
